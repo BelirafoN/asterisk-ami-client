99a72ff7273944b74181082e6457c41f13dc8f14
Alex Voronyansky <belirafon@gmail.com>
2016-05-11 13:15:50 +0300

    added new tests

--- test/amiClientTest.js
+++ test/amiClientTest.js
@@ -416,6 +416,15 @@ describe('Ami Client internal functionality', function(){
             });
         });
 
+        it('Last response not have $time field after resolving promissabled action without ActionID', done => {
+            client.connect(USERNAME, SECRET, {port: socketOptions.port}).then(() => {
+                client.action({Action: 'Ping'}, true).then(response => {
+                    assert.ok(response.$time === undefined);
+                    done();
+                });
+            });
+        });
+
     });
 
     describe('Client\'s configuration', function(){
